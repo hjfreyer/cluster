58a306ddf7da1de8db8de371318d6caa97614c44
Hunter Freyer <yt@hjfreyer.com>
2015-04-13 21:03:19 -0400

    Remove the base image. Didn't actually help things.

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-echo $TRANSMISSION_HTTP_SERVICE_HOST
-echo $TRANSMISSION_HTTP_SERVICE_PORT
-
-sed "s/@TRANSMISSION_HOSTPORT@/$TRANSMISSION_HTTP_SERVICE_HOST:$TRANSMISSION_HTTP_SERVICE_PORT/" /etc/nginx/nginx.conf > /tmp/conf
-mv /tmp/conf /etc/nginx/nginx.conf
+sed -i "s/@TRANSMISSION_HOSTPORT@/$TRANSMISSION_HTTP_SERVICE_HOST:$TRANSMISSION_HTTP_SERVICE_PORT/" /etc/nginx/nginx.conf
 
 exec nginx
--- docker/rebuild-base.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-docker build --no-cache -t hjfreyer/base base
-./rebuild.sh
