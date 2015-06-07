1d331a0f725317ddcef1f86e57001078d94bcf1b
Hunter Freyer <yt@hjfreyer.com>
2015-06-07 01:10:42 -0400

    Updates for new host

--- all_up.sh
+++ all_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-python2 mksecret.py sslcerts $HOME/keys/hjfreyer.com-cert/2015/assembled/ | kubectl create -f -
+python2 mksecret.py sslcerts /data/hjfreyer/keys/hjfreyer.com-cert/2015/assembled/ | kubectl create -f -
 
 kubectl create -f k8s/services.yaml
 kubectl create -f k8s/transmission.yaml
