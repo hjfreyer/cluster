88ffc340c8fd15ce4fefe0b368a459cadfdd7fa7
Hunter Freyer <yt@hjfreyer.com>
2015-08-31 22:19:27 -0400

    More secrets!

--- all_up.sh
+++ all_up.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
-python2 mksecret.py sslcerts /data/hjfreyer/keys/hjfreyer.com-cert/2015/assembled/ | kubectl create -f -
-python2 mksecret.py ddclient-creds /data/hjfreyer/keys/google-domains/ | kubectl create -f -
+KEYS=/data/hjfreyer/keys/
+python mksecret.py sslcerts $KEYS/hjfreyer.com-cert/2015/assembled/ | kubectl create -f -
+python mksecret.py ddclient-creds $KEYS/google-domains/ | kubectl create -f -
+python mksecret.py btcd-creds $KEYS/btcd/ |kubectl create -f -
 
 kubectl create -f k8s/services.yaml
 kubectl create -f k8s/transmission.yaml
 kubectl create -f k8s/nginx.yaml
+kubectl create -f k8s/ddclient.yaml
