929cc422625a7a4670e12bd340fc4106e9854980
Hunter Freyer <yt@hjfreyer.com>
2015-12-31 22:28:13 -0500

    Refactored

--- hypervisor/hypervisor.go
+++ hypervisor/hypervisor.go
@@ -3,182 +3,211 @@ package main
 //go:generate go-bindata data/
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"path"
-	"strconv"
-	"sync"
-	"time"
+
+	"github.com/codegangsta/cli"
 )
 
-type disk struct {
-	Path string
+// type disk struct {
+// 	Path string
+// }
+
+// type machine struct {
+// 	MemoryMb int
+// 	Mac      string
+// 	Disks    []*disk
+// }
+
+// type context struct {
+// 	TempDir  string
+// 	Machines []*machine
+// 	Disks    []*disk
+// }
+
+// func (c *context) InitLeibniz() {
+// 	c.Machines = append(c.Machines, m)
+// 	c.Disks = append(c.Disks, d)
+// }
+
+// const (
+// 	coreImgPath     = "/virt/images/coreos/"
+// 	cloudConfigPath = "data/cloud_config.yml"
+// )
+
+// func (c *context) InitCore() {
+// 	hash := cloudConfigHash()
+// 	path := path.Join(coreImgPath, hash+".qcow2")
+// 	if _, err := os.Stat(path); os.IsNotExist(err) {
+// 		if err := initCoreOsDrive(path); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	} else if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	d := &disk{path}
+// 	m := &machine{
+// 		MemoryMb: 1024,
+// 		Mac:      "96:03:08:82:1C:02",
+// 		Disks:    []*disk{d},
+// 	}
+// 	c.Machines = append(c.Machines, m)
+// 	c.Disks = append(c.Disks, d)
+// }
+
+// func initCoreOsDrive(path string) error {
+// 	return nil
+// }
+
+// func cloudConfigHash() string {
+// 	hash := sha256.Sum256(MustAsset(cloudConfigPath))
+// 	return hex.Dump(hash[:])
+// }
+
+// func (c *context) getCloudConfig() (string, error) {
+// 	if err := RestoreAsset(c.TempDir, cloudConfigPath); err != nil {
+// 		return "", err
+// 	}
+// 	return path.Join(c.TempDir, cloudConfigPath), nil
+// }
+
+// func (c *context) RunAll() {
+// 	var cmds []*exec.Cmd
+// 	for _, m := range c.Machines {
+// 		cmd := getQemuCmd(m)
+// 		cmd.Stdout = os.Stdout
+// 		cmd.Stderr = os.Stderr
+// 		if err := cmd.Start(); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		cmds = append(cmds, cmd)
+// 	}
+
+// 	for _, cmd := range cmds {
+// 		if err := cmd.Wait(); err != nil {
+// 			log.Print(err)
+// 		}
+// 	}
+// }
+
+// const nbdPath = "/usr/bin/qemu-nbd"
+
+// func installDrive(d *disk) (string, error) {
+// 	path := "/dev/nbd" + nextSuffix()
+// 	cmd := exec.Command(nbdPath, "-c", path, d.Path)
+// 	if err := cmd.Run(); err != nil {
+// 		return "", err
+// 	}
+// 	return path, nil
+// }
+
+// func uninstallDrive(path string) error {
+// 	cmd := exec.Command(nbdPath, "-d", path)
+// 	return cmd.Run()
+// }
+
+// func getQemuCmd(m *machine) *exec.Cmd {
+// 	const qemuBin = "/usr/bin/qemu-system-x86_64"
+// 	args := []string{
+// 		"-enable-kvm",
+// 		"-nographic",
+// 		"-m", fmt.Sprintf("%dM", m.MemoryMb),
+// 		"-net", "nic,macaddr=" + m.Mac,
+// 		"-net", "bridge,br=br0",
+// 	}
+
+// 	for i, d := range m.Disks {
+// 		args = append(args, "-drive",
+// 			fmt.Sprintf("file=%s,index=%d,media=disk", d.Path, i))
+// 	}
+
+// 	return exec.Command(qemuBin, args...)
+// }
+
+type command struct {
+	tempDir string
 }
 
-type machine struct {
-	MemoryMb int
-	Mac      string
-	Disks    []*disk
-}
-
-type context struct {
-	TempDir  string
-	Machines []*machine
-	Disks    []*disk
-}
-
-func (c *context) InitLeibniz() {
-	d := &disk{
-		"/virt/images/leibniz.qcow2",
+func (c *command) parseMachine(ctx *cli.Context) *Machine {
+	name := ctx.Args().Get(0)
+	if name == "" {
+		log.Fatal("Must specify a <machine>")
 	}
-	m := &machine{
-		MemoryMb: 1024,
-		Mac:      "96:03:08:82:1C:01",
-		Disks:    []*disk{d},
+	m := GetMachine(ctx.Args().Get(0))
+	if m == nil {
+		log.Fatalf("No machine named %q", name)
 	}
-	c.Machines = append(c.Machines, m)
-	c.Disks = append(c.Disks, d)
+	return m
 }
 
-const (
-	coreImgPath     = "/virt/images/coreos/"
-	cloudConfigPath = "data/cloud_config.yml"
-)
-
-func (c *context) InitCore() {
-	hash := cloudConfigHash()
-	path := path.Join(coreImgPath, hash+".qcow2")
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		if err := initCoreOsDrive(path); err != nil {
-			log.Fatal(err)
-		}
-	} else if err != nil {
+func (c *command) status(ctx *cli.Context) {
+	machine := c.parseMachine(ctx)
+	ready, err := Ready(machine)
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	d := &disk{path}
-	m := &machine{
-		MemoryMb: 1024,
-		Mac:      "96:03:08:82:1C:02",
-		Disks:    []*disk{d},
-	}
-	c.Machines = append(c.Machines, m)
-	c.Disks = append(c.Disks, d)
-}
-
-func initCoreOsDrive(path string) error {
-	return nil
-}
-
-func cloudConfigHash() string {
-	hash := sha256.Sum256(MustAsset(cloudConfigPath))
-	return hex.Dump(hash[:])
-}
-
-func (c *context) getCloudConfig() (string, error) {
-	if err := RestoreAsset(c.TempDir, cloudConfigPath); err != nil {
-		return "", err
-	}
-	return path.Join(c.TempDir, cloudConfigPath), nil
-}
-
-func (c *context) RunAll() {
-	var cmds []*exec.Cmd
-	for _, m := range c.Machines {
-		cmd := getQemuCmd(m)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Start(); err != nil {
-			log.Fatal(err)
-		}
-		cmds = append(cmds, cmd)
-	}
-
-	for _, cmd := range cmds {
-		if err := cmd.Wait(); err != nil {
-			log.Print(err)
-		}
+	if !ready {
+		log.Fatal("Machine not ready")
 	}
 }
 
-// NOTE: Code stolen from ioutil/tempfile.go
-//
-// Random number state.
-// We generate random temporary file names so that there's a good
-// chance the file doesn't exist yet - keeps the number of tries in
-// TempFile to a minimum.
-var rand uint32
-var randmu sync.Mutex
-
-func reseed() uint32 {
-	return uint32(time.Now().UnixNano() + int64(os.Getpid()))
+func (c *command) init(ctx *cli.Context) {
+	machine := c.parseMachine(ctx)
+	if machine != Coreos {
+		log.Fatal("I only know how to setup the coreos box")
+	}
+	if err := InitCoreos(c.tempDir); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func nextSuffix() string {
-	randmu.Lock()
-	r := rand
-	if r == 0 {
-		r = reseed()
+func (c *command) run(ctx *cli.Context) {
+	machine := c.parseMachine(ctx)
+	ready, err := Ready(machine)
+	if err != nil {
+		log.Fatal(err)
 	}
-	r = r*1664525 + 1013904223 // constants from Numerical Recipes
-	rand = r
-	randmu.Unlock()
-	return strconv.Itoa(int(1e9 + r%1e9))[1:]
-}
-
-const nbdPath = "/usr/bin/qemu-nbd"
-
-func installDrive(d *disk) (string, error) {
-	path := "/dev/nbd" + nextSuffix()
-	cmd := exec.Command(nbdPath, "-c", path, d.Path)
-	if err := cmd.Run(); err != nil {
-		return "", err
+	if !ready {
+		log.Fatal("Machine not ready")
 	}
-	return path, nil
-}
-
-func uninstallDrive(path string) error {
-	cmd := exec.Command(nbdPath, "-d", path)
-	return cmd.Run()
-}
-
-func getQemuCmd(m *machine) *exec.Cmd {
-	const qemuBin = "/usr/bin/qemu-system-x86_64"
-	args := []string{
-		"-enable-kvm",
-		"-nographic",
-		"-m", fmt.Sprintf("%dM", m.MemoryMb),
-		"-net", "nic,macaddr=" + m.Mac,
-		"-net", "bridge,br=br0",
-	}
-
-	for i, d := range m.Disks {
-		args = append(args, "-drive",
-			fmt.Sprintf("file=%s,index=%d,media=disk", d.Path, i))
-	}
-
-	return exec.Command(qemuBin, args...)
+	log.Fatal(Run(machine))
 }
 
 func main() {
-	var c context
-
 	td, err := ioutil.TempDir("", "hypervisor")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	c.TempDir = td
+	var c command
+	c.tempDir = td
 
-	c.InitLeibniz()
-	c.RunAll()
+	app := cli.NewApp()
+	app.Commands = []cli.Command{
+		{
+			Name:   "status",
+			Action: c.status,
+		},
+		{
+			Name:   "init",
+			Action: c.init,
+		},
+		{
+			Name:   "run",
+			Action: c.run,
+		},
+	}
 
-	os.RemoveAll(c.TempDir)
+	app.Run(os.Args)
+
+	// var c context
+
+	// c.InitLeibniz()
+	// c.RunAll()
+
+	os.RemoveAll(c.tempDir)
 }
 
 // exec  \
---
+++ hypervisor/machines.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const (
+	baseDir    = "/virt/"
+	diskSuffix = ".qcow2"
+
+	qemuBin    = "/usr/bin/qemu-system-x86_64"
+	nbdBin     = "/usr/bin/qemu-nbd"
+	qemuImgBin = "/usr/bin/qemu-img"
+
+	cloudConfigPath   = "data/cloud_config.yml"
+	coreosInstallPath = "data/coreos-install"
+)
+
+type Machine struct {
+	Name     string
+	MemoryMb int
+	Mac      string
+	Disks    []*Disk
+}
+
+type Disk struct {
+	Name string
+}
+
+type MachineSet struct {
+	Machines []*Machine
+}
+
+var Leibniz = &Machine{
+	Name:     "leibniz",
+	MemoryMb: 1024,
+	Mac:      "96:03:08:82:1C:01",
+	Disks:    []*Disk{{"main"}},
+}
+
+var Coreos = &Machine{
+	Name:     "coreos",
+	MemoryMb: 1024,
+	Mac:      "96:03:08:82:1C:02",
+	Disks:    []*Disk{{"main"}},
+}
+
+var Machines = []*Machine{Leibniz, Coreos}
+
+func GetMachine(name string) *Machine {
+	for _, m := range Machines {
+		if m.Name == name {
+			return m
+		}
+	}
+	return nil
+}
+
+func getCloudConfigHash() string {
+	hash := sha256.Sum256(MustAsset(cloudConfigPath))
+	return hex.EncodeToString(hash[:])
+}
+
+func Ready(m *Machine) (bool, error) {
+	for i := 0; i < len(m.Disks); i++ {
+		if exists, err := DiskExists(m, i); err != nil {
+			return false, err
+		} else if !exists {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func DiskExists(m *Machine, index int) (bool, error) {
+	if _, err := os.Stat(diskPath(m, index)); os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func Run(m *Machine) error {
+	args := []string{
+		"-enable-kvm",
+		"-nographic",
+		"-m", fmt.Sprintf("%dM", m.MemoryMb),
+		"-net", "nic,macaddr=" + m.Mac,
+		"-net", "bridge,br=br0",
+	}
+
+	for i := range m.Disks {
+		args = append(args, "-drive",
+			fmt.Sprintf("file=%s,index=%d,media=disk", diskPath(m, i), i))
+	}
+
+	cmd := exec.Command(qemuBin, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func redirect(cmd *exec.Cmd) {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+}
+
+func InitCoreos(tmpdir string) error {
+	disk := diskPath(Coreos, 0)
+
+	if exists, err := DiskExists(Coreos, 0); err != nil {
+		return err
+	} else if !exists {
+		return errors.New("Disk does not exist")
+	}
+
+	// Install the virtual disk.
+
+	// This could be smarter and find an unused file but whatever.
+	const devPath = "/dev/nbd0"
+	nbdCmd := exec.Command(nbdBin, "-c", devPath, disk)
+	redirect(nbdCmd)
+	if err := nbdCmd.Run(); err != nil {
+		return err
+	}
+	defer func() {
+		exec.Command(nbdBin, "-d", devPath).Run()
+	}()
+
+	// Mount the disk
+	mntPath := path.Join(tmpdir, "nbdmnt")
+	if err := os.Mkdir(mntPath, 0777); err != nil {
+		return err
+	}
+
+	mntCmd := exec.Command("mount", devPath, mntPath)
+	redirect(mntCmd)
+	if err := mntCmd.Run(); err != nil {
+		return err
+	}
+	defer func() {
+		exec.Command("umount", mntPath).Run()
+	}()
+
+	cloudConfigDest := path.Join(mntPath, "var/lib/coreos-install")
+	if err := os.MkdirAll(path.Dir(cloudConfigDest), 0777); err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(cloudConfigDest, MustAsset(cloudConfigPath), 0666); err != nil {
+		return err
+	}
+	return nil
+}
+
+// func InitCoreos(m *Machine, idx int, tmpdir string) error {
+// 	disk := diskPath(m, idx)
+
+// 	if exists, err := DiskExists(m, idx); err != nil {
+// 		return err
+// 	} else if exists {
+// 		return errors.New("cowardly refusing to overwrite disk")
+// 	}
+
+// 	createCmd := exec.Command(qemuImgBin, "create", "-f", "qcow2", disk, fmt.Sprintf("%dG", m.Disks[idx].SizeGb))
+
+// 	if err := createCmd.Run(); err != nil {
+// 		return err
+// 	}
+
+// 	// sudo qemu-img create -f qcow2 /images/leibniz.qcow2 100G
+
+// 	devPath := "/dev/nbd0"
+// 	nbdCmd := exec.Command(nbdBin, "-c", devPath, disk)
+// 	nbdCmd.Stdout = os.Stdout
+// 	nbdCmd.Stderr = os.Stderr
+// 	if err := nbdCmd.Run(); err != nil {
+// 		return err
+// 	}
+// 	defer func() {
+// 		exec.Command(nbdBin, "-d", devPath).Run()
+// 	}()
+
+// 	if err := RestoreAsset(tmpdir, coreosInstallPath); err != nil {
+// 		return err
+// 	}
+
+// 	installer := path.Join(tmpdir, coreosInstallPath)
+// 	installCmd := exec.Command(installer, "-d", devPath)
+// 	installCmd.Stdout = os.Stdout
+// 	installCmd.Stderr = os.Stderr
+
+// 	return installCmd.Run()
+// }
+
+func diskPath(m *Machine, index int) string {
+	return path.Join(baseDir, m.Name, m.Disks[index].Name+diskSuffix)
+}
+
+// NOTE: Code stolen from ioutil/tempfile.go
+//
+// Random number state.
+// We generate random temporary file names so that there's a good
+// chance the file doesn't exist yet - keeps the number of tries in
+// TempFile to a minimum.
+var rand uint32
+var randmu sync.Mutex
+
+func reseed() uint32 {
+	return uint32(time.Now().UnixNano() + int64(os.Getpid()))
+}
+
+func nextSuffix() string {
+	randmu.Lock()
+	r := rand
+	if r == 0 {
+		r = reseed()
+	}
+	r = r*1664525 + 1013904223 // constants from Numerical Recipes
+	rand = r
+	randmu.Unlock()
+	return strconv.Itoa(int(1e9 + r%1e9))[1:]
+}
